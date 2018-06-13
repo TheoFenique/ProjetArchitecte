b946a4812bb6fd1f28658eb246af2712e6a3f073
Théo Fénique <theo.fenique@gmail.com>
2018-06-14 01:01:14 +0200

    Homepage terminée
    Manque plus que le responsive.

---
+++ templates/header.php
@@ -0,0 +1,36 @@
+<nav class="menu">
+  <ul>
+    <li>
+    <a href="#" title="Acceuil">Accueil</a>
+      <ul>
+        <li><a href="#">Page d'acceuil</a></li>
+        <li><a href="#">Liste des articles</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#" title="news">News</a>
+      <ul>
+        <li><a href="#">Tout savoir sur la Nasa</a></li>
+        <li><a href="#">Liste des actualités</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#" title="Forum">Forum</a>
+      <ul>
+        <li><a href="#">Acceder aux forums</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#" title="Plus">Plus</a>
+      <ul>
+        <li><a href="#">Mentions legales</a></li>
+        <li><a href="#">A propos</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#" title="Connexion" id="connexion">Connexion</a>
+    </li>
+      </ul>
+    </li>
+  </ul>
+</nav>
